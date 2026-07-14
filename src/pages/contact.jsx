e20870c3a3f64b495a1fe1cf6cc1ebe62bfd0886
MohamadAl-Zohbi <alzbya0123@gmail.com>
2026-07-14 21:49:16 +0300

    api whatsapp

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -1,42 +1,52 @@
 import { useState } from "react";
 
+ 
+function sendMessage(){
+console.log(name)
+}
+  export default function Contact() {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [message, setMessage] = useState('');
 
-export default function Contact() {
-  return (
-    <section className="min-h-screen bg-black text-white py-20 px-6">
-
-      <div className="max-w-6xl mx-auto">
 
 
-        {/* Header */}
 
-        <div className="text-center mb-16">
+    return (
+      <section className="min-h-screen bg-black text-white py-20 px-6">
 
-          <h1 className="
+        <div className="max-w-6xl mx-auto">
+
+
+          {/* Header */}
+
+          <div className="text-center mb-16">
+
+            <h1 className="
             text-5xl md:text-6xl
             font-extrabold
           ">
-            Contact <span className="text-blue-500">
-              Me
-            </span>
-          </h1>
+              Contact <span className="text-blue-500">
+                Me
+              </span>
+            </h1>
 
 
-          <p className="
+            <p className="
             mt-5
             text-gray-400
             text-lg
           ">
-            Have a project, opportunity, or just want to talk about technology?
-            Feel free to reach out.
-          </p>
+              Have a project, opportunity, or just want to talk about technology?
+              Feel free to reach out.
+            </p>
 
-        </div>
-
-
+          </div>
 
 
-        <div className="
+
+
+          <div className="
           grid
           md:grid-cols-2
           gap-10
@@ -44,9 +54,9 @@ export default function Contact() {
 
 
 
-          {/* Contact Information */}
+            {/* Contact Information */}
 
-          <div className="
+            <div className="
             bg-gradient-to-br
             from-blue-600/20
             to-transparent
@@ -59,141 +69,145 @@ export default function Contact() {
           ">
 
 
-            <h2 className="
+              <h2 className="
               text-3xl
               font-bold
               mb-8
             ">
-              Get In Touch
-            </h2>
+                Get In Touch
+              </h2>
 
 
 
-            <div className="space-y-6">
+              <div className="space-y-6">
 
 
-              <div>
+                <div>
 
-                <h3 className="text-blue-400 font-semibold">
-                  📧 Email
-                </h3>
+                  <h3 className="text-blue-400 font-semibold">
+                    📧 Email
+                  </h3>
 
-                <p className="text-gray-300">
-                  mohamadalzohbi01@gmail.com
-                </p>
-
-              </div>
-
-
-
-              <div>
-
-                <h3 className="text-blue-400 font-semibold">
-                  📱 Phone
-                </h3>
-
-                <p className="text-gray-300">
-                  +961 71 081 432
-                </p>
-
-              </div>
-
-
-
-              <div>
-
-                <h3 className="text-blue-400 font-semibold">
-                  🌍 Location
-                </h3>
-
-                <p className="text-gray-300">
-                  Lebanon
-                </p>
-
-              </div>
-
-
-
-              <div>
-
-                <h3 className="text-blue-400 font-semibold">
-                  💻 Social
-                </h3>
-
-                <div className="flex gap-4 mt-3">
-
-
-                  <a
-                    href="#"
-                    className="
-                    px-4 py-2
-                    border border-blue-500
-                    rounded-lg
-                    hover:bg-blue-500
-                    transition
-                    "
-                  >
-                    GitHub
-                  </a>
-
-
-
-                  <a
-                    href="#"
-                    className="
-                    px-4 py-2
-                    border border-blue-500
-                    rounded-lg
-                    hover:bg-blue-500
-                    transition
-                    "
-                  >
-                    LinkedIn
-                  </a>
-
+                  <p className="text-gray-300">
+                    mohamadalzohbi01@gmail.com
+                  </p>
 
                 </div>
 
-              </div>
 
+
+                <div>
+
+                  <h3 className="text-blue-400 font-semibold">
+                    📱 Phone
+                  </h3>
+
+                  <p className="text-gray-300">
+                    +961 71 081 432
+                  </p>
+
+                </div>
+
+
+
+                <div>
+
+                  <h3 className="text-blue-400 font-semibold">
+                    🌍 Location
+                  </h3>
+
+                  <p className="text-gray-300">
+                    Lebanon
+                  </p>
+
+                </div>
+
+
+
+                <div>
+
+                  <h3 className="text-blue-400 font-semibold">
+                    💻 Social
+                  </h3>
+
+                  <div className="flex gap-4 mt-3">
+
+
+                    <a
+                      href="#"
+                      className="
+                    px-4 py-2
+                    border border-blue-500
+                    rounded-lg
+                    hover:bg-blue-500
+                    transition
+                    "
+                    >
+                      GitHub
+                    </a>
+
+
+
+                    <a
+                      href="#"
+                      className="
+                    px-4 py-2
+                    border border-blue-500
+                    rounded-lg
+                    hover:bg-blue-500
+                    transition
+                    "
+                    >
+                      LinkedIn
+                    </a>
+
+
+                  </div>
+
+                </div>
+
+
+
+              </div>
 
 
             </div>
 
 
-          </div>
 
 
 
+            {/* Message Form */}
 
-
-          {/* Message Form */}
-
-          <div className="
-            bg-white
+            <div className="
+            bg-gradient-to-br
+            from-blue-600/20
+border
+            border-blue-500/30
             text-black
             rounded-3xl
             p-8
           ">
 
 
-            <h2 className="
+              <h2 className="
               text-3xl
               font-bold
               mb-8
+              text-white
             ">
-              Send Message
-            </h2>
+                Send Message
+              </h2>
 
 
 
-            <div className="space-y-5">
+              <div className="space-y-5">
 
 
-              <input
-                type="text"
-                placeholder="Your Name"
-                className="
+                <input
+                  type="text"
+                  placeholder="Your Name"
+                  className="
                 w-full
                 p-4
                 rounded-xl
@@ -202,14 +216,16 @@ export default function Contact() {
                 focus:ring-2
                 focus:ring-blue-500
                 "
-              />
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+                />
 
 
 
-              <input
-                type="email"
-                placeholder="Your Email"
-                className="
+                <input
+                  type="email"
+                  placeholder="Your Email"
+                  className="
                 w-full
                 p-4
                 rounded-xl
@@ -218,14 +234,16 @@ export default function Contact() {
                 focus:ring-2
                 focus:ring-blue-500
                 "
-              />
+                value={email}
+                onChange={e => setEmail(e.target.value)}
+                />
 
 
 
-              <textarea
-                rows="5"
-                placeholder="Your Message"
-                className="
+                <textarea
+                  rows="5"
+                  placeholder="Your Message"
+                  className="
                 w-full
                 p-4
                 rounded-xl
@@ -234,11 +252,13 @@ export default function Contact() {
                 focus:ring-2
                 focus:ring-blue-500
                 "
-              />
+                value={message}
+                onChange={e => setMessage(e.target.value)}
+                />
 
 
-              <button
-                className="
+                <button
+                  className="
                 w-full
                 py-4
                 bg-blue-600
@@ -248,9 +268,13 @@ export default function Contact() {
                 hover:bg-blue-700
                 transition
                 "
-              >
-                <a href="#">Send Messag</a>
-              </button>
+                onClick={sendMessage}
+                >
+                  Send Message
+                </button>
+
+
+              </div>
 
 
             </div>
@@ -259,14 +283,11 @@ export default function Contact() {
           </div>
 
 
-        </div>
 
 
+          {/* Footer Message */}
 
-
-        {/* Footer Message */}
-
-        <div className="
+          <div className="
           mt-20
           text-center
           border-t
@@ -274,19 +295,20 @@ export default function Contact() {
           pt-10
         ">
 
-          <p className="
+            <p className="
             text-xl
             text-gray-400
           ">
-            I'm always open to new opportunities,
-            collaborations, and interesting projects.
-          </p>
+              I'm always open to new opportunities,
+              collaborations, and interesting projects.
+            </p>
+
+          </div>
+
 
         </div>
 
+      </section>
+    );
+  }
 
-      </div>
-
-    </section>
-  );
-}
